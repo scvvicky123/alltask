2db924dbc32573ce56e6ea2ca5ce1a32605a1326
origin <scvvicky@gmail.com>
2024-07-04 20:55:10 +0530

    Script file of task

---
+++ scriptdir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ 
+mkdir India
+mkdir -p India/Telagana , India/Andhrapradesh , India/Karnataka
+mkdir -p India/Telagana/hyderabad , India/Telagana/suryapet , India/Telagana/nalgonda
+touch India/Telagana/hyderabad/a1.txt , India/Telagana/hyderabad/a2.txt , India/Telagana/hyderabad/a3.txt
+touch India/Telagana/suryapet/a1.txt , India/Telagana/suryapet/a2.txt , India/Telagana/suryapet/a3.txt
+touch India/Telagana/nalgonda/a1.txt , India/Telagana/nalgonda/a2.txt , India/Telagana/nalgonda/a3.txt
+mkdir -p India/Andhrapradesh/vijaywada , India/Andhrapradesh/westgodavari , India/Andhrapradesh/eastgodavari
+touch India/Andhrapradesh/vijaywada/b1.txt India/Andhrapradesh/vijaywada/b2.txt India/Andhrapradesh/vijaywada/b3.txt
+touch India/Andhrapradesh/westgodavari/b1.txt India/Andhrapradesh/westgodavari/b2.txt India/Andhrapradesh/westgodavari/b3.txt
+touch India/Andhrapradesh/eastgodavari/b1.txt India/Andhrapradesh/eastgodavari/b2.txt India/Andhrapradesh/eastgodavari/b3.txt
+mkdir -p India/Karnataka/bangalore India/Karnataka/mysore India/Karnataka/hubli
+touch India/Karnataka/bangalore/c1.txt India/Karnataka/bangalore/c2.txt India/Karnataka/bangalore/c3.txt
+touch India/Karnataka/mysore/c1.txt India/Karnataka/mysore/c2.txt India/Karnataka/mysore/c3.txt
+touch India/Karnataka/hubli/c1.txt India/Karnataka/hubli/c2.txt India/Karnataka/hubli/c3.txt
+
+
